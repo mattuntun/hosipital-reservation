5a15fc5a4f35ed868e06f9bcf91545787d54958a
mattuntun <mattuntun3@gmail.com>
2019-10-16 17:41:55 +0900

    患者情報確認画面を追加

---
+++ hospital/patient_registration_change_deletion/new_patient_registration/pt_Confirmation/pt_Confirmation_page.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html><!--このサイトはhtmlだよ-->
+<html lang="ja"><!--ここからhtmlが始まるよ-->
+  <head><!--このサイトの設定だよ-->
+    <meta charset="utf-8"><!--このサイトの構成文字はutf-8だよ-->
+    
+    <meta name="description" content="予約アプリ"><!--descriptionは概要・説明の事 その後に説明文を入れてあげる　　入力！！！！-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"><!--イイ感じに携帯でも縮小してくれる-->
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"><!--IEでもいい感じに表示してくれる-->
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+ 
+    <!-- Bootstrap Javascript(jQuery含む) -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>    
+   
+
+    <link rel="stylesheet" href="css/style.css"><!--ファビコン(サイトのバーの画像)の種類(拡張子)と場所　　　　　入力！！！！-->
+
+   <!--適用するとアウトライン表示<style>
+    *{
+      outline: 1px solid #ff0000;
+    }
+    </style>-->
+    <title></title><!--サイトのバータイトルを　　入力！！！-->
+  </head><!--ここで設定終了-->
+  
+  <body><!--ここから本文が始まるよ-->
+  <div class="wrapper"><!--body全体のclassをラッパーとする-->
+
+    <header><!--ここからヘッダー始まるよ-->
+    <h1>患者登録情報確認</h1>
+       
+
+    </header><!--ここでヘッダー終わり-->
+    <main><!--メイン始まる-->
+    
+
+
+    </main>
+    <footer><!--フッターだよ-->
+    
+
+
+
+    </footer><!--フッター終わり-->
+
+    
+    <section><!--本文内のカテゴリーだよ-->
+      
+    </section><!--本文内のカテゴリー終わり-->
+
+    <section><!--本文内のカテゴリーだよ-->
+    
+    </section><!--本文内のカテゴリー終わり-->
+    
+    <section><!--本文内のカテゴリーだよ-->
+    
+    </section><!--本文内のカテゴリー終わり-->
+    
+
+
+
+  </div><!--body全体のclassをラッパー終わり-->
+  </body><!--ここで本文終わりだよ-->
+
+</html><!--ここでhtmlが終わるよ-->
+
+
+
+
+
+
+
+
+
+
+
